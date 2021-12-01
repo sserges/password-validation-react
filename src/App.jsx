f721bd974ee3cef1b95bf7510c0f146d88578392
sserges <agounkeserge@gmail.com>
2021-11-30 20:20:43 -0400

    css box-sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react'
-import classes from './App.module.css'
+import './App.scss'
 
 function App() {
   return (
-    <>
-      <h1 className={`${classes.momo} ${classes.small}`}>Password Validator</h1>
+    <div className="cool">
+      <h1>Password Validator</h1>
 
       <input type="password" placeholder="Please enter your password" />
       <button>Show</button>
 
-      <ul className={classes.small}>
+      <ul>
         <li>One lowercase letter</li>
         <li>One uppercase letter</li>
         <li>One special character</li>
         <li>One number</li>
         <li>Minimum 8 characters</li>
       </ul>
-    </>
+    </div>
   )
 }
 
