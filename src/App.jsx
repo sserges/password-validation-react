951efb4f1d9ee545078dde24380b9101e0be0f2b
sserges <agounkeserge@gmail.com>
2021-12-01 20:59:01 -0400

    Dynamic icon change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 import './App.scss'
 
+import openEye from './eye.svg'
+import closedEye from './eye-off.svg'
+
 function App() {
 
   const [passwordIsVisible, setPasswordIsVisible] = useState(false)
 
   return (
-    <div className="cool">
+    <>
       <h1>Password Validator</h1>
 
       <input type={passwordIsVisible ? 'text' : 'password'} placeholder="Please enter your password" />
-      <button type="button" onClick={() => setPasswordIsVisible((prevState) => !prevState)}>
-        {passwordIsVisible ? 'Hide' : 'Show'}
-      </button>
+      <span onClick={() => setPasswordIsVisible((prevState) => !prevState)}>
+        <img src={passwordIsVisible ? closedEye : openEye} alt={passwordIsVisible ? 'Closed Eye' : 'open Eye'} width="32" />
+      </span>
 
       <ul>
         <li>One lowercase letter</li>
@@ -21,7 +24,7 @@ function App() {
         <li>One number</li>
         <li>Minimum 8 characters</li>
       </ul>
-    </div>
+    </>
   )
 }
 
