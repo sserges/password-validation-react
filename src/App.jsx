e6e782d53850976eaa8b8814601bb40b96c21cd9
sserges <agounkeserge@gmail.com>
2021-12-01 20:45:23 -0400

    Show / hide password with React (Part 2)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 import './App.scss'
 
 function App() {
+
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
+
   return (
     <div className="cool">
       <h1>Password Validator</h1>
 
-      <input type="password" placeholder="Please enter your password" />
-      <button>Show</button>
+      <input type={passwordIsVisible ? 'text' : 'password'} placeholder="Please enter your password" />
+      <button type="button" onClick={() => setPasswordIsVisible((prevState) => !prevState)}>
+        {passwordIsVisible ? 'Hide' : 'Show'}
+      </button>
 
       <ul>
         <li>One lowercase letter</li>
